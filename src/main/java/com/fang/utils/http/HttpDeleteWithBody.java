076129e95bbb726b3eb3771cc2b122a53cfff76c
方鹏辉 <fangpenghui@one-sports.com>
2023-02-13 11:20:22 +0800

    引入Hutool工具包 引入http工具类，以后可以以此发起请求 引入常量

---
+++ src/main/java/com/fang/utils/http/HttpDeleteWithBody.java
@@ -0,0 +1,32 @@
+package com.fang.utils.http;
+
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+
+import java.net.URI;
+
+/**
+ * @author chenjp
+ */
+public class HttpDeleteWithBody extends HttpEntityEnclosingRequestBase {
+
+    public static final String METHOD_NAME = "DELETE";
+
+    public HttpDeleteWithBody() {
+        super();
+    }
+
+    public HttpDeleteWithBody(final String uri) {
+        super();
+        setURI(URI.create(uri));
+    }
+
+    public HttpDeleteWithBody(final URI uri) {
+        super();
+        setURI(uri);
+    }
+
+    @Override
+    public String getMethod() {
+        return METHOD_NAME;
+    }
+}
