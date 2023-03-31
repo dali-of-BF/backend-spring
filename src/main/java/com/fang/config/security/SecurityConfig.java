25368684b845f273d3a255837767f4a1d610330f
FPH <fangpenghui@one-sports.com>
2023-03-31 17:50:12 +0800

    fix 排除一系列无需校验的网址

--- src/main/java/com/fang/config/security/SecurityConfig.java
+++ src/main/java/com/fang/config/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.fang.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * security配置
@@ -14,10 +18,43 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+//    /**
+//     * @param auth
+//     * @throws Exception
+//     */
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("fang")
+//                .password(passwordEncoder().encode("123456"))
+//                .authorities("/*");
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll().
+        http.authorizeRequests().anyRequest().authenticated().
                 and().logout().permitAll()
                 .and().csrf().disable();//关闭CSRF保护即可。
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+//                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .antMatchers("/swagger-ui/**")
+                .antMatchers("/test/**")
+                .antMatchers("/i18n/**")
+                .antMatchers("/doc.html")
+                .antMatchers("/swagger-resources/**")
+                .antMatchers("/api-docs*")
+                .antMatchers("/webjars/**")
+                .antMatchers("/v2/api-docs")
+                .antMatchers("/druid/**")
+                .antMatchers("/captcha/**");
     }
 }
