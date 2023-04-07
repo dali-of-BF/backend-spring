a04e3f6dd39188e917f2bf8485b2680d8c1678d5
FPH <fangpenghui@one-sports.com>
2023-04-07 17:07:24 +0800

    feat 构建token接口

--- src/main/java/com/fang/security/TokenProvider.java
+++ src/main/java/com/fang/security/TokenProvider.java
@@ -1,11 +1,27 @@
 package com.fang.security;
 
+import org.springframework.security.core.Authentication;
+
 /**
  * 令牌提供接口
  * @author FPH
  * @since 2022年9月24日21:03:08
  */
 public interface TokenProvider {
+    /**
+     * 创建token
+     * @param authentication
+     * @param prefix 前缀
+     * @param rememberMe
+     * @return
+     */
+    String createToken(Authentication authentication,String prefix, boolean rememberMe);
 
-
+    /**
+     * 通过token获取
+     * @param token
+     * @param clientCode
+     * @return
+     */
+    Authentication getAuthentication(String token,String clientCode);
 }
