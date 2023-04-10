7a17806883d478d747efa851a79e00780807c2f6
FPH <fangpenghui@one-sports.com>
2023-04-10 23:43:42 +0800

    feat 鉴权处理类、自定义拒绝访问处理程序

---
+++ src/main/java/com/fang/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,40 @@
+package com.fang.security;
+
+import com.fang.common.HttpStatus;
+import com.fang.common.result.Result;
+import com.fang.utils.JsonMapper;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author FPH
+ * 自定义拒绝访问处理程序
+ */
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    /**
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param e
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void handle(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse,
+                       AccessDeniedException e) throws IOException, ServletException {
+        Result<Object> result = new Result<>();
+        result.setCode(HttpStatus.FORBIDDEN);
+        result.setMessage(e.getMessage());
+        result.getError().setCode(HttpStatus.FORBIDDEN);
+        result.getError().setMessage(e.getMessage());
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.setContentType("application/json;charset=UTF-8");
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        httpServletResponse.getWriter().append(JsonMapper.writeValueAsString(result));
+    }
+}
