b098f82327815b9a9644b4394e57afa1fb46ff19
FPH <fangpenghui@one-sports.com>
2023-04-18 17:13:23 +0800

    feat swagger分组

--- src/main/java/com/fang/config/swagger/SwaggerConfig.java
+++ src/main/java/com/fang/config/swagger/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.fang.config.swagger;
 
+import com.fang.constants.SwaggerGroupConstants;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.Getter;
@@ -36,14 +37,15 @@ public class SwaggerConfig {
     private String host=null;
 
     @Bean
-    public Docket createRestApi() {
+    public Docket DocketAccount() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .groupName(SwaggerGroupConstants.ACCOUNT_API)
                 .select()
                 /**
                  * 扫描包
                  */
-                .apis(RequestHandlerSelectors.basePackage("com.fang.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.fang.controller.account"))
                 /**
                  * 扫描在API注解的contorller
                  */
@@ -59,6 +61,80 @@ public class SwaggerConfig {
                 .build().host(host);
     }
 
+    @Bean
+    public Docket DocketMonitor() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .groupName(SwaggerGroupConstants.MONITOR_API)
+                .select()
+                /**
+                 * 扫描包
+                 */
+                .apis(RequestHandlerSelectors.basePackage("com.fang.controller.monitor"))
+                /**
+                 * 扫描在API注解的contorller
+                 */
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                /**
+                 * 扫描带ApiOperation注解的方法
+                 */
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                /**
+                 * 自定义host
+                 */
+                .build().host(host);
+    }
+
+    @Bean
+    public Docket DocketResource() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .groupName(SwaggerGroupConstants.RESOURCE_API)
+                .select()
+                /**
+                 * 扫描包
+                 */
+                .apis(RequestHandlerSelectors.basePackage("com.fang.controller.resource"))
+                /**
+                 * 扫描在API注解的contorller
+                 */
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                /**
+                 * 扫描带ApiOperation注解的方法
+                 */
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                /**
+                 * 自定义host
+                 */
+                .build().host(host);
+    }
+
+    @Bean
+    public Docket DocketTest() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .groupName(SwaggerGroupConstants.TEST_API)
+                .select()
+                /**
+                 * 扫描包
+                 */
+                .apis(RequestHandlerSelectors.basePackage("com.fang.controller.test"))
+                /**
+                 * 扫描在API注解的contorller
+                 */
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                /**
+                 * 扫描带ApiOperation注解的方法
+                 */
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                /**
+                 * 自定义host
+                 */
+                .build().host(host);
+    }
     private ApiInfo apiInfo() {
         String version = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
         return new ApiInfoBuilder()
