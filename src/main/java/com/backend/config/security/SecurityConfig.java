54465258baffe57e33d441c116a813af12389952
FPH <fangpenghui@one-sports.com>
2023-04-21 17:56:42 +0800

    feat 注册接口不鉴权

--- src/main/java/com/backend/config/security/SecurityConfig.java
+++ src/main/java/com/backend/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.backend.security.tokenProvider.RedisTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.BeanIds;
@@ -71,24 +72,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and()
-                .logout().logoutUrl("/auth/logout")
-                .logoutSuccessHandler(new LogoutSuccessHandle(redisTokenProvider)).and()
-                //关闭CSRF保护
-                .csrf().disable()
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new UsernamePasswordAuthenticationFilterImpl(this.authenticationManager(),redisTokenProvider),UsernamePasswordAuthenticationFilter.class)
-                .addFilter(new TokenAuthenticationFilter(this.authenticationManager(),redisTokenProvider,unAuthenticationEntryPoint))
-                .headers()
-                .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN).and()
-                //将策略设置为禁止使用以下功能：地理位置、MIDI、同步 XHR、麦克风、相机、磁力计、陀螺仪和付款。它允许全屏模式，但只允许在同一源中使用。
-                .featurePolicy("geolocation 'none'; midi 'none'; sync-xhr 'none'; microphone 'none'; camera 'none'; magnetometer 'none'; gyroscope 'none'; fullscreen 'self'; payment 'none'");
+        http.logout().logoutUrl("/auth/logout")
+            .logoutSuccessHandler(new LogoutSuccessHandle(redisTokenProvider)).and()
+            //关闭CSRF保护
+            .csrf().disable()
+            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(new UsernamePasswordAuthenticationFilterImpl(this.authenticationManager(),redisTokenProvider),UsernamePasswordAuthenticationFilter.class)
+            .addFilter(new TokenAuthenticationFilter(this.authenticationManager(),redisTokenProvider,unAuthenticationEntryPoint))
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .headers()
+            .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN).and()
+            //将策略设置为禁止使用以下功能：地理位置、MIDI、同步 XHR、麦克风、相机、磁力计、陀螺仪和付款。它允许全屏模式，但只允许在同一源中使用。
+            .featurePolicy("geolocation 'none'; midi 'none'; sync-xhr 'none'; microphone 'none'; camera 'none'; magnetometer 'none'; gyroscope 'none'; fullscreen 'self'; payment 'none'");
     }
 
     @Override
     public void configure(WebSecurity web) {
         web.ignoring()
-//                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .antMatchers(HttpMethod.OPTIONS, "/**")
                 .antMatchers("/swagger-ui/**")
                 .antMatchers("/test/**")
                 .antMatchers("/i18n/**")
@@ -98,6 +101,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/webjars/**")
                 .antMatchers("/v2/api-docs")
                 .antMatchers("/druid/**")
-                .antMatchers("/captcha/**");
+                .antMatchers("/captcha/**")
+                .antMatchers("/auth/register");
     }
 }
