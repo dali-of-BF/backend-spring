1ce075d3b0f4375041bd8cf2217eba679715444a
FPH <fangpenghui@one-sports.com>
2023-05-22 18:11:49 +0800

    feat 少了service注解

--- src/main/java/com/backend/service/sys/SysRoleService.java
+++ src/main/java/com/backend/service/sys/SysRoleService.java
@@ -3,12 +3,14 @@ package com.backend.service.sys;
 import com.backend.domain.entity.sys.SysRole;
 import com.backend.mapper.sys.SysRoleMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
 /**
  * @author FPH
  * @since 2023年5月22日18:10:12
  *
  */
+@Service
 public class SysRoleService extends ServiceImpl<SysRoleMapper, SysRole> {
 
 }
