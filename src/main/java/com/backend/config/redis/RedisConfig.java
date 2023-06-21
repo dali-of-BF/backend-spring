f4039ad9e2d9e85ffed0553712918ba0355e3a99
FPH <fangpenghui@one-sports.com>
2023-06-21 15:58:24 +0800

    Revert "fix redis序列化改为jdk序列化"
    This reverts commit e6b8a7aec8b787679cbd2355bfa2d4560822f473.

--- src/main/java/com/backend/config/redis/RedisConfig.java
+++ src/main/java/com/backend/config/redis/RedisConfig.java
@@ -1,60 +1,40 @@
 package com.backend.config.redis;
 
-import lombok.AllArgsConstructor;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @author FPH
  * @since 2023年4月20日15:01:18
  */
-@EnableCaching
 @Configuration
-@AllArgsConstructor
-@AutoConfigureBefore(RedisConnectionFactory.class)
 public class RedisConfig {
 
-    private final RedisConnectionFactory redisConnectionFactory;
-
+    /**
+     *
+     * @param connectionFactory
+     * @return
+     */
     @Bean
-    @Primary
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
-        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        return redisTemplate;
-    }
+    @SuppressWarnings(value = { "unchecked", "rawtypes" })
+    public <T,V> RedisTemplate<T, V> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<T, V> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
 
-    @Bean("redisJsonTemplate")
-    public RedisTemplate<String, Object> redisJsonTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
-        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        return redisTemplate;
-    }
+        FastJsonRedisSerializer serializer = new FastJsonRedisSerializer(Object.class);
 
-    @Bean("redisStringTemplate")
-    public RedisTemplate<String, Object> redisStringTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        return redisTemplate;
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        // Hash的key也采用StringRedisSerializer的序列化方式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
     }
 }
