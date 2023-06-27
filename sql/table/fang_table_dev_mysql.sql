f2b38579b7d6c99fabc0bee5a11d510593ef54f6
FPH <fangpenghui@one-sports.com>
2023-06-27 10:53:18 +0800

    feat 角色表新增系统标识

--- sql/table/fang_table_dev_mysql.sql
+++ sql/table/fang_table_dev_mysql.sql
@@ -67,7 +67,8 @@ CREATE TABLE `sys_role`  (
                              PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
-SET FOREIGN_KEY_CHECKS = 1;
+ALTER TABLE `base_spring`.`sys_role`
+    ADD COLUMN `system_code` varchar(10) NULL COMMENT '系统标识' AFTER `last_modified_date`;
 
 
 -- ----------------------------
