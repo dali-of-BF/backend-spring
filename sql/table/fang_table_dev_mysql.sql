f5eba2068f87aca7c74be58d5592cd09fb297b2b
FPH <fangpenghui@one-sports.com>
2023-07-25 09:46:18 +0800

    fix 设置时区

--- sql/table/fang_table_dev_mysql.sql
+++ sql/table/fang_table_dev_mysql.sql
@@ -17,6 +17,11 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+set global time_zone = '+8:00'; ##修改mysql全局时区为北京时间，即我们所在的东8区
+set time_zone = '+8:00'; ##修改当前会话时区
+flush privileges; #立即生效
+
+
 -- ----------------------------
 -- Table structure for sys_account
 -- ----------------------------
