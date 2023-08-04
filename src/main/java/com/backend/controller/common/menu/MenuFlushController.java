1bd4c99f989c6c5e4c9d51e9b581d81c2ad3591e
FPH <fangpenghui@one-sports.com>
2023-08-04 18:01:41 +0800

    feat 未完，菜单自动化构建雏形

---
+++ src/main/java/com/backend/controller/common/menu/MenuFlushController.java
@@ -0,0 +1,36 @@
+package com.backend.controller.common.menu;
+
+import com.backend.annotation.Log;
+import com.backend.common.result.Result;
+import com.backend.constants.ApiPathConstants;
+import com.backend.domain.entity.sys.SysMenu;
+import com.backend.service.sys.SysMenuService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author FPH
+ * @since 2023年8月4日16:48:41
+ * 菜单重构控制器
+ */
+@RestController
+@Slf4j
+@RequestMapping(ApiPathConstants.COM_MENU)
+@RequiredArgsConstructor
+@Api(tags = "菜单重构控制器")
+@Log
+public class MenuFlushController {
+    private final SysMenuService sysMenuService;
+    @GetMapping("/refactor-menu")
+    @ApiOperation("重构菜单")
+    public Result<List<SysMenu>> refactorMenu() {
+        return Result.success(sysMenuService.refactorMenu()) ;
+    }
+}
--- src/main/java/com/backend/service/sys/SysMenuService.java
+++ src/main/java/com/backend/service/sys/SysMenuService.java
@@ -1,17 +1,40 @@
 package com.backend.service.sys;
 
 import com.backend.domain.entity.sys.SysMenu;
+import com.backend.domain.entity.sys.SysResource;
+import com.backend.enums.sys.AppIdEnum;
 import com.backend.mapper.sys.SysMenuMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.google.common.collect.Lists;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author FPH
  */
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class SysMenuService extends ServiceImpl<SysMenuMapper, SysMenu> {
+    private final SysSourceService sysSourceService;
 
-    public void saveMenu(){
+    public List<SysMenu> refactorMenu() {
+        List<SysMenu> menuList = Lists.newArrayList();
+        Set<SysResource> resource = sysSourceService.getResource();
+        Map<String, List<SysResource>> appToResourceMap =
+                resource.stream().filter(i -> AppIdEnum.getAllAppIdType().contains(i.getAppId()))
+                        .collect(Collectors.groupingBy(SysResource::getAppId));
+        appToResourceMap.forEach((i, j) -> {
+            SysMenu sysMenu = new SysMenu();
 
+            menuList.add(sysMenu);
+        });
+        return menuList;
     }
 }
